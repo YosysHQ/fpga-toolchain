54b68a6cef5d9e4cd8b9bc5ab5ece38d99bc77d1
Ed Bordin <edbordin@gmail.com>
2020-04-25 14:42:16 +1000

    add build-data I missed

---
+++ build-data/darwin/convert.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+
+# (c) 2012-2016 Anaconda, Inc. / https://anaconda.com
+# All Rights Reserved
+#
+# conda is distributed under the terms of the BSD 3-clause license.
+# Consult LICENSE.txt or http://opensource.org/licenses/BSD-3-Clause.
+'''
+We use the following conventions in this module:
+
+    dist:        canonical package name, e.g. 'numpy-1.6.2-py26_0'
+
+    ROOT_PREFIX: the prefix to the root environment, e.g. /opt/anaconda
+
+    PKGS_DIR:    the "package cache directory", e.g. '/opt/anaconda/pkgs'
+                 this is always equal to ROOT_PREFIX/pkgs
+
+    prefix:      the prefix of a particular environment, which may also
+                 be the root environment
+
+Also, this module is directly invoked by the (self extracting) tarball
+installer to create the initial environment, therefore it needs to be
+standalone, i.e. not import any other parts of `conda` (only depend on
+the standard library).
+'''
+import os
+import re
+import sys
+import stat
+
+on_win = bool(sys.platform == 'win32')
+try:
+    FORCE = bool(int(os.getenv('FORCE', 0)))
+except ValueError:
+    FORCE = False
+
+def exp_backoff_fn(fn, *args):
+    """
+    for retrying file operations that fail on Windows due to virus scanners
+    """
+    if not on_win:
+        return fn(*args)
+
+    import time
+    import errno
+    max_tries = 6  # max total time = 6.4 sec
+    for n in range(max_tries):
+        try:
+            result = fn(*args)
+        except (OSError, IOError) as e:
+            if e.errno in (errno.EPERM, errno.EACCES):
+                if n == max_tries - 1:
+                    raise Exception("max_tries=%d reached" % max_tries)
+                time.sleep(0.1 * (2 ** n))
+            else:
+                raise e
+        else:
+            return result
+
+
+class PaddingError(Exception):
+    pass
+
+
+def binary_replace(data, a, b):
+    """
+    Perform a binary replacement of `data`, where the placeholder `a` is
+    replaced with `b` and the remaining string is padded with null characters.
+    All input arguments are expected to be bytes objects.
+    """
+    def replace(match):
+        occurances = match.group().count(a)
+        padding = (len(a) - len(b)) * occurances
+        if padding < 0:
+            raise PaddingError(a, b, padding)
+        return match.group().replace(a, b) + b'\0' * padding
+
+    pat = re.compile(re.escape(a) + b'([^\0]*?)\0')
+    res = pat.sub(replace, data)
+    assert len(res) == len(data)
+    return res
+
+
+def update_prefix(path, new_prefix, placeholder, mode):
+    if on_win:
+        # force all prefix replacements to forward slashes to simplify need
+        # to escape backslashes - replace with unix-style path separators
+        new_prefix = new_prefix.replace('\\', '/')
+
+    path = os.path.realpath(path)
+    with open(path, 'rb') as fi:
+        data = fi.read()
+    if mode == 'text':
+        new_data = data.replace(placeholder.encode('utf-8'),
+                                new_prefix.encode('utf-8'))
+    elif mode == 'binary':
+        if on_win:
+            # anaconda-verify will not allow binary placeholder on Windows.
+            # However, since some packages might be created wrong (and a
+            # binary placeholder would break the package, we just skip here.
+            return
+        new_data = binary_replace(data, placeholder.encode('utf-8'),
+                                  new_prefix.encode('utf-8'))
+    else:
+        sys.exit("Invalid mode:" % mode)
+
+    if new_data == data:
+        return
+    st = os.lstat(path)
+    # unlink in case the file is memory mapped
+    os.unlink(path)
+    with open(path, 'wb') as fo:
+        fo.write(new_data)
+    os.chmod(path, stat.S_IMODE(st.st_mode))
+
+if len(sys.argv) != 2:
+	print("Usage: {} <prefix>".format(sys.argv[0]))
+	sys.exit(1)
+new_prefix=sys.argv[1]
+with open("info/has_prefix", "r") as f:
+    for l in f:
+        (prefix, t, filename) = l.rstrip().split(" ")
+        print("patching conda file {}".format(filename))
+        update_prefix(filename, new_prefix, prefix, t)
