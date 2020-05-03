4c853151fd5a2bd7c99a1b09bbfeb445826c3642
Ed Bordin <edbordin@gmail.com>
2020-05-03 18:46:04 +1000

    try using azure pipelines to build bba, disable travis

---
+++ build-bba.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+##################################
+#   FPGA toolchain builder       #
+##################################
+
+set -e
+
+# Set english language for propper pattern matching
+export LC_ALL=C
+
+# export VERSION="${TRAVIS_TAG}"
+export VERSION=nightly
+
+# -- Target architectures
+export ARCH=linux_x86_64
+
+# -- Toolchain name
+export NAME=ecp5-bba
+
+# -- Store current dir
+export WORK_DIR=$PWD
+# -- Folder for building the source code
+export BUILDS_DIR=$WORK_DIR/_builds
+# -- Folder for storing the generated packages
+export PACKAGES_DIR=$WORK_DIR/_packages
+# --  Folder for storing the source code from github
+export UPSTREAM_DIR=$WORK_DIR/_upstream
+
+# -- Create the build directory
+mkdir -p $BUILDS_DIR
+# -- Create the packages directory
+mkdir -p $PACKAGES_DIR
+# -- Create the upstream directory and enter into it
+mkdir -p $UPSTREAM_DIR
+
+# -- Directory for compiling the tools
+export BUILD_DIR=$BUILDS_DIR/build_$ARCH
+
+# -- Directory for installation the target files
+export PACKAGE_DIR=$PACKAGES_DIR/build_$ARCH
+
+# -- Create the build dir
+mkdir -p $BUILD_DIR
+
+# -- Create the package folders
+mkdir -p $PACKAGE_DIR/$NAME/bin
+mkdir -p $PACKAGE_DIR/$NAME/share
+
+# -- Test script function
+function test_bin {
+  . $WORK_DIR/test/test_bin.sh $1
+  if [ $? != "0" ]; then
+    exit 1
+  fi
+}
+
+# -- Print function
+function print {
+  echo ""
+  echo $1
+  echo ""
+}
+
+if [ $INSTALL_DEPS == "1" ]; then
+  print ">> Install dependencies"
+  . $WORK_DIR/scripts/install_dependencies.sh
+fi
+
+print ">> Set build flags"
+. $WORK_DIR/scripts/build_setup.sh
+
+print ">> Compile nextpnr-ecp5-bba"
+$WORK_DIR/scripts/compile_nextpnr-ecp5-bba.sh
+
+print ">> Create package"
+. $WORK_DIR/scripts/create_package.sh
---
+++ scripts/compile_nextpnr-ecp5-bba.sh
@@ -0,0 +1,84 @@
+#!/bin/bash -x
+# -- Compile nextpnr-ecp5 script
+
+set -e
+
+nextpnr_dir=nextpnr-ecp5
+nextpnr_uri=https://github.com/YosysHQ/nextpnr.git
+nextpnr_commit=master
+nextpnr_commit=$(git ls-remote ${nextpnr_uri} ${nextpnr_commit} | cut -f 1)
+
+prjtrellis_dir=prjtrellis
+prjtrellis_uri=https://github.com/SymbiFlow/prjtrellis.git
+# Every time you update this, regenerate the chipdb files!
+prjtrellis_commit=master
+prjtrellis_commit=$(git ls-remote ${prjtrellis_uri} ${prjtrellis_commit} | cut -f 1)
+
+# -- Setup
+. $WORK_DIR/scripts/build_setup.sh
+
+cd $UPSTREAM_DIR
+
+# -- Clone the sources from github
+test -e $nextpnr_dir || git clone $nextpnr_uri $nextpnr_dir
+git -C $nextpnr_dir fetch
+git -C $nextpnr_dir checkout $nextpnr_commit
+git -C $nextpnr_dir log -1
+
+test -e $prjtrellis_dir || git clone $prjtrellis_uri $prjtrellis_dir
+git -C $prjtrellis_dir fetch
+git -C $prjtrellis_dir checkout $prjtrellis_commit
+git -C $prjtrellis_dir submodule init
+git -C $prjtrellis_dir submodule update
+git -C $prjtrellis_dir log -1
+
+# -- Copy the upstream sources into the build directory
+mkdir -p $BUILD_DIR/$nextpnr_dir
+mkdir -p $BUILD_DIR/$prjtrellis_dir
+rsync -a $nextpnr_dir $BUILD_DIR --exclude .git
+rsync -a $prjtrellis_dir $BUILD_DIR --exclude .git
+
+cd $BUILD_DIR/
+
+if [ -e $nextpnr_dir/CMakeCache.txt -o -e $prjtrellis_dir/CMakeCache.txt ]
+then
+    echo "CMakeCache.txt exists!"
+fi
+rm -f $nextpnr_dir/CMakeCache.txt $prjtrellis_dir/CMakeCache.txt
+
+# -- Compile it
+cd $BUILD_DIR/$prjtrellis_dir/libtrellis
+
+# The first run of the build produces the Python shared library
+# (Disabled since we now use PREGENERATED_BBA_PATH)
+mkdir -p $BUILD_DIR/$prjtrellis_dir/tmp_prjtrellis_install
+cmake \
+    -DBUILD_SHARED=ON \
+    -DCMAKE_INSTALL_PREFIX=$BUILD_DIR/$prjtrellis_dir/tmp_prjtrellis_install \
+    -DSTATIC_BUILD=OFF \
+    -DBoost_USE_STATIC_LIBS=OFF \
+    -DBUILD_PYTHON=ON \
+    -DCURRENT_GIT_VERSION=$prjtrellis_commit \
+    .
+make -j$J CXX="$CXX"
+make install
+rm -rf CMakeCache.txt
+
+cd $BUILD_DIR/$nextpnr_dir
+        # -DPREGENERATED_BBA_PATH=$BUILD_DIR/chipdb
+cmake \
+    -DARCH=ecp5 \
+    -DTRELLIS_INSTALL_PREFIX=$BUILD_DIR/$prjtrellis_dir/tmp_prjtrellis_install \
+    -DBUILD_HEAP=ON \
+    -DBUILD_GUI=OFF \
+    -DBUILD_PYTHON=ON \
+    -DSTATIC_BUILD=OFF \
+    -DBoost_USE_STATIC_LIBS=OFF \
+    .
+
+# skip most of the nextpnr build and generate the *.bba chipdb files
+# (also needlessly runs bbasm to generate *.cc files and compiles them but for now we'll let it)
+make -j$J CXX="$CXX" ecp5_chipdb 
+
+mkdir -p $PACKAGE_DIR/$NAME/bba
+cp $BUILD_DIR/$nextpnr_dir/ecp5/chipdbs/*.bba $PACKAGE_DIR/$NAME/bba
