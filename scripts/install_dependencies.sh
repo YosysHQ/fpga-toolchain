da1fe5f87b5aae0960c5bbedb7fc8256e2e3ada7
Ed Bordin <edbordin@gmail.com>
2020-05-06 18:22:17 +1000

    see if we can get /usr/include back

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -103,6 +103,7 @@ if [ $ARCH == "windows_amd64" ]; then
 fi
 
 if [ $ARCH == "darwin" ]; then
+    open /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg
     # match the version that was installed on travis CI
     sudo xcode-select -s /Applications/Xcode_9.4.1.app/Contents/Developer
     wget https://repo.anaconda.com/miniconda/Miniconda3-4.7.12.1-MacOSX-x86_64.sh -O miniconda.sh
