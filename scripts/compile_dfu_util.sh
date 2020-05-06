412410b8586140514e3e7060ef5100bb4fbaca67
Ed Bordin <edbordin@gmail.com>
2020-05-06 18:10:16 +1000

    try to fix autotools build of dfu-util

--- scripts/compile_dfu_util.sh
+++ scripts/compile_dfu_util.sh
@@ -21,14 +21,13 @@ rsync -a $dfu_util $BUILD_DIR --exclude .git
 cd $BUILD_DIR/$dfu_util
 
 ./autogen.sh
-
 # -- Compile it
 if [ $ARCH == "darwin" ]; then
-    ./configure
+    ./configure CFLAGS="$CFLAGS -isystem $(xcrun --sdk macosx --show-sdk-path)/usr/include/"
     make LDFLAGS="/tmp/conda/lib/libusb-1.0.a -Wl,-framework,IOKit -Wl,-framework,CoreFoundation $LDFLAGS"
 else
-    ./configure
-    make CFLAGS="-I$WORK_DIR/build-data/include/libusb-1.0" LIBS="-static $WORK_DIR/build-data/lib/$ARCH/libusb-1.0.a -lpthread"
+    ./configure USB_CFLAGS="-I$WORK_DIR/build-data/include/libusb-1.0" USB_LIBS="-static $WORK_DIR/build-data/lib/$ARCH/libusb-1.0.a -lpthread"
+    make
 fi
 
 TOOLS="dfu-util dfu-prefix dfu-suffix"
