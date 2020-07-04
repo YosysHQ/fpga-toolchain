3c11cfdf0fbacc38793a29df8f846120969f5647
Ed Bordin <edbordin@gmail.com>
2020-07-04 14:51:41 +1000

    #20 source script to pass through vars

--- build_bba.sh
+++ build_bba.sh
@@ -14,7 +14,7 @@ INSTALL_DEPS=1
 . scripts/_common.sh linux_x86_64
 
 print ">> Compile nextpnr-ecp5-bba"
-$WORK_DIR/scripts/compile_nextpnr_ecp5_bba.sh
+. $WORK_DIR/scripts/compile_nextpnr_ecp5_bba.sh
 
 print ">> Create package"
 . $WORK_DIR/scripts/create_package.sh
