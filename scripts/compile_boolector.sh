213c713234996285ce97f05d527b0fa0910360f4
Ed Bordin <edbordin@gmail.com>
2020-07-11 18:26:59 +1000

    #25 see if windows builds now

--- scripts/compile_boolector.sh
+++ scripts/compile_boolector.sh
@@ -11,17 +11,23 @@ git_clone $dir_name $git_url $commit
 cd $BUILD_DIR/$dir_name
 
 $SED -i 's|\./configure.sh -fPIC|\./configure.sh -fPIC -static|;' ./contrib/setup-btor2tools.sh
+$SED -i 's/MINGW32/MINGW/;' ./contrib/setup-utils.sh # fix windows detection on MINGW64
+
 ./contrib/setup-btor2tools.sh
 ./contrib/setup-lingeling.sh
-./configure.sh
 
 if [ $ARCH = "darwin" ]
 then
+    ./configure.sh
     $MAKE -C build -j$J
 elif [ ${ARCH:0:7} = "windows" ]
 then
+    # this is easier than working out how to escape an arg with a space
+    $SED -i 's/cmake .. $cmake_opts/cmake -G "MinGW Makefiles" .. $cmake_opts/;' ./configure.sh
+
     $MAKE -C build -j$J
 else
+    ./configure.sh
     $MAKE -C build -j$J
 fi
 
