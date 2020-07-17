72aac3d724794127063a7df0f77953dce337ad7b
Ed Bordin <edbordin@gmail.com>
2020-07-17 22:19:19 +1000

    #25 try different sby test case

--- scripts/test/test_sby.sh
+++ scripts/test/test_sby.sh
@@ -8,21 +8,33 @@ commit=master
 git_url=https://github.com/YosysHQ/SymbiYosys.git
 
 git_clone_direct $dir_name $git_url $commit
+mkdir -p ~/Work
+rm -rf  ~/Work/picorv32
+git clone https://github.com/cliffordwolf/picorv32.git ~/Work/picorv32
+cd $BUILD_DIR/$dir_name/sbysrc
+sby demo1.sby
+# # cd $BUILD_DIR/$dir_name/docs/examples/quickstart
+# # smtbmc
+# echo ==============COVER
+# sby -f cover.sby
+# # smtbmc
+# echo ==============DEMO
+# sby -f demo.sby
+# # smtbmc boolector
+# echo ==============MEMORY
+# sby -f memory.sby
+# # smtbmc
+# echo ==============PROVE
+# sby -f prove.sby
 
-cd $BUILD_DIR/$dir_name/docs/examples/quickstart
-# smtbmc
-sby cover.sby
-# smtbmc
-sby demo.sby
-# smtbmc boolector
-sby memory.sby
-# smtbmc
-sby prove.sby
+# cd ../puzzles
+# # yices
+# echo ==============HASh
+# sby -f djb2hash.sby
+# # # z3
+# # echo ==============PRIME
+# # sby -f primegen.sby
 
-cd ../puzzles
-# yices
-sby djb2hash.sby
-
-cd ../demos
-# z3
-sby fib.sby
+# cd ../demos
+# # z3
+# sby fib.sby
