5bf590436e492dc686a07140446702de90ccee6e
Ed Bordin <edbordin@gmail.com>
2020-07-22 20:29:50 +1000

    #25 get sby tests running again

--- scripts/test/test_sby.sh
+++ scripts/test/test_sby.sh
@@ -7,7 +7,7 @@ dir_name=sby_test
 commit=master
 git_url=https://github.com/YosysHQ/SymbiYosys.git
 
-# git_clone_direct $dir_name $git_url $commit
+git_clone_direct $dir_name $git_url $commit
 # mkdir -p ~/Work
 # rm -rf  ~/Work/picorv32
 # git clone https://github.com/cliffordwolf/picorv32.git ~/Work/picorv32
@@ -37,4 +37,4 @@ sby -f primegen.sby
 
 cd ../demos
 # z3
-sby fib.sby
+# sby fib.sby
