07403122b45c5b4f535f84c8c63d080a4d4c2d28
Ed Bordin <edbordin@gmail.com>
2020-07-25 15:46:22 +1000

    #13 OFL seems to compile on all three platforms, still need to test with some hardware and get patches upstream before we ship it

--- scripts/compile_openfpgaloader.sh
+++ scripts/compile_openfpgaloader.sh
@@ -3,48 +3,31 @@
 set -e
 
 dir_name=openfpgaloader
-commit=master
-git_url=https://github.com/trabucayre/openFPGALoader.git
+commit=cxxopts_migration
+git_url=https://github.com/edbordin/openFPGALoader.git
 
 git_clone $dir_name $git_url $commit
 
 mkdir -p $BUILD_DIR/$dir_name/build
-cd $BUILD_DIR/$dir_name
+cd $BUILD_DIR/$dir_name/build
 
 
 # -- Compile it
 if [ $ARCH == "darwin" ]; then
-patch -p1 < $WORK_DIR/scripts/openfpgaloader.diff
-    cd build
     cmake -DENABLE_UDEV=OFF ../
     $MAKE -j$J
-    # ./configure --libdir=/opt/local/lib \
-    #     --includedir=/opt/local/include \
-    #     USB_CFLAGS="-I$LIBUSB_ROOT/include/libusb-1.0" \
-    #     USB_LIBS="$LIBUSB_ROOT/lib/libusb-1.0.a -Wl,-framework,IOKit -Wl,-framework,CoreFoundation"
 elif [ ${ARCH:0:7} = "windows" ]
 then
-    cd build
     cmake -G "MinGW Makefiles" -DENABLE_UDEV=OFF -DBUILD_STATIC=ON ../
     $MAKE -j$J
-    # ./configure USB_LIBS="-static -lpthread -lusb-1.0"
-    # $MAKE
 else
-# $WORK_DIR/build-data/lib/$ARCH/libusb-1.0.a $WORK_DIR/build-data/lib/$ARCH/libftdi1.a
-# -L$WORK_DIR/build-data/lib/$ARCH
-#  -I$WORK_DIR/build-data/include/libusb-1.0
-# LDFLAGS="-static   -lpthread" 
-        # -DCMAKE_EXE_LINKER_FLAGS="-lpthread -lrt"
-    patch -p1 < $WORK_DIR/scripts/openfpgaloader.diff
-    cd build
-    VERBOSE=1 \
-        cmake -DENABLE_UDEV=OFF -DBUILD_STATIC=ON \
+    cmake -DLINK_CMAKE_THREADS=ON -DUSE_PKGCONFIG=OFF -DENABLE_UDEV=OFF -DBUILD_STATIC=ON \
         -DLIBUSB_LIBRARIES=$WORK_DIR/build-data/lib/$ARCH/libusb-1.0.a \
         -DLIBFTDI_LIBRARIES=$WORK_DIR/build-data/lib/$ARCH/libftdi1.a \
         -DLIBFTDI_VERSION=1.4 \
         -DCMAKE_CXX_FLAGS="-I$WORK_DIR/build-data/include/libusb-1.0 -I$WORK_DIR/build-data/include/libftdi1" \
         ../
-    VERBOSE=1 $MAKE -j$J
+    $MAKE -j$J
 fi
 
 test_bin openFPGALoader$EXE
