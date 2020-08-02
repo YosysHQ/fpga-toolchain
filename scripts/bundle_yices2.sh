a3afaaef63c00a19d0cc313bedf8f559363c3ec6
Ed Bordin <edbordin@gmail.com>
2020-08-02 20:38:40 +1000

    #25 try to fix mingw build, test binaries

--- scripts/bundle_yices2.sh
+++ scripts/bundle_yices2.sh
@@ -10,15 +10,19 @@ git_clone $dir_name $git_url $commit
 cd $BUILD_DIR/$dir_name
 autoconf
 ./configure
-$MAKE -j$J static-bin
+
 
 if [ $ARCH == "darwin" ]
 then
+    $MAKE -j$J static-bin
     YICES2_BINDIR=./build/x86_64-apple-darwin*-release/static_bin
 elif [ ${ARCH:0:7} = "windows" ]
 then
-    YICES2_BINDIR=./build/x86_64-pc-mingw32-release/static_bin
+    mv configs/make.include.x86_64-w64-mingw32 configs/make.include.x86_64-pc-mingw64
+    $MAKE -j$J static-bin
+    YICES2_BINDIR=./build/x86_64-pc-mingw64-release/static_bin
 else
+    $MAKE -j$J static-bin
     YICES2_BINDIR=./build/x86_64-pc-linux-gnu-release/static_bin
 fi
 
@@ -27,6 +31,11 @@ cp $YICES2_BINDIR/yices_sat$EXE $PACKAGE_DIR/$NAME/bin/yices-sat$EXE
 cp $YICES2_BINDIR/yices_smt$EXE $PACKAGE_DIR/$NAME/bin/yices-smt$EXE
 cp $YICES2_BINDIR/yices_smt2$EXE $PACKAGE_DIR/$NAME/bin/yices-smt2$EXE
 
-strip_binaries bin/{yices,yices-sat,yices-smt,yices-smt2}$EXE
+TOOLS="yices,yices-sat,yices-smt,yices-smt2"
+
+for tool in $TOOLS; do
+  test_bin $PACKAGE_DIR/$NAME/bin/$tool$EXE
+  strip_binaries bin/$tool$EXE
+done
 
 clean_build $dir_name
