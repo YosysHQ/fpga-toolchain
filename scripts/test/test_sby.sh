fb92597e150e9c36058c268b72d2a0412b460afb
Ed Bordin <edbordin@gmail.com>
2020-08-06 18:26:18 +1000

    #25 run tests again with -e on

--- scripts/test/test_sby.sh
+++ scripts/test/test_sby.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/env bash
-# -- Test binaries execute with --help
 
-set +e
+set -e
 
 dir_name=sby_test
 commit=master
 git_url=https://github.com/YosysHQ/SymbiYosys.git
 
 git_clone_direct $dir_name $git_url $commit
-# mkdir -p ~/Work
-# rm -rf  ~/Work/picorv32
-# git clone https://github.com/cliffordwolf/picorv32.git ~/Work/picorv32
-# cd $BUILD_DIR/$dir_name/sbysrc
-# sby demo1.sby
 cd $BUILD_DIR/$dir_name/docs/examples/quickstart
 # smtbmc
 echo ==============COVER
@@ -44,5 +38,3 @@ echo exitcode: $?
 cd ../demos
 # z3
 # sby fib.sby
-
-set -e
