061be3f0c77caafbb8fafabcfc0045e8b74aeefd
Ed Bordin <edbordin@gmail.com>
2020-11-22 11:57:32 +1100

    #46 fix armv7l arch strings

--- scripts/test/test_entrypoint.sh
+++ scripts/test/test_entrypoint.sh
@@ -7,15 +7,17 @@ export ARCH=$1
 if [ ${ARCH} == "linux_armv7l" ] || [ ${ARCH} == "linux_aarch64" ]
 then
     if [ ${ARCH} == "linux_armv7l" ]; then
-        DOCKER_ARCH="arm"
+        QUS_ARCH="arm"
+        DOCKER_IMAGE="arm32v7/ubuntu:16.04"
     else
-        DOCKER_ARCH="aarch64"
+        QUS_ARCH="aarch64"
+        DOCKER_IMAGE="aarch64/ubuntu:16.04"
     fi
 
     # QUS allows us to easily run an arm container (uses QEMU behind the scenes)
-    sudo docker run --rm --privileged aptman/qus -s -- -p $DOCKER_ARCH
+    sudo docker run --rm --privileged aptman/qus -s -- -p $QUS_ARCH
     docker run --env VERSION -v `pwd`:/fpga-toolchain -w /fpga-toolchain \
-        $DOCKER_ARCH/ubuntu:16.04 /fpga-toolchain/scripts/test/run_tests.sh $ARCH
+        $DOCKER_IMAGE /fpga-toolchain/scripts/test/run_tests.sh $ARCH
 else
     ./scripts/test/run_tests.sh $ARCH
 fi
